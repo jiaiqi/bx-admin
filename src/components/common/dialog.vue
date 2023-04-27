bbd6157454e26699d15f8a82474307302c673b9b
ho <hong349@qq.com>
2023-04-27 20:08:42 +0800

    引入批量编辑组件

--- src/components/common/dialog.vue
+++ src/components/common/dialog.vue
@@ -67,6 +67,11 @@
       </select-fill-grid>
     </el-dialog>
 
+    <el-dialog title="批量选择操作" width="90%" :close-on-click-modal="1==2" :visible="activeForm == 'batch-edit-grid'" @close="activeForm = 'xx'" append-to-body >
+      <select-fill-grid v-if="activeForm == 'select_fill_grid'" @closeDialog="closeDialog" :gridData="gridData" :_service="_service" :buttonInfo="buttonInfo">
+      </select-fill-grid>
+    </el-dialog>
+
     <!--列表加form操作-->
     <el-dialog title="列表" width="90%" :close-on-click-modal="1==2" :visible="activeForm == 'list_update'" @close="activeForm = 'xx'" append-to-body >
 
