cde933b5e7c79bd0403e40b2189180aa86ed8407
ho <hong349@qq.com>
2023-04-27 19:44:47 +0800

    批量处理方法

---
+++ src/components/mixin/batch-add-mixin.js
@@ -0,0 +1,15 @@
+export default {
+    data(){
+        return {
+
+        }
+    },
+    methods: {
+        buildBatchConfig(button){
+            console.log(button)
+        },
+  
+    }
+  
+  };
+  
