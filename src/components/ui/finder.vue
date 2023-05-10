d590af9bb47a64a9d1603ade77f05c4577f984c7
jiaqi <2468260248@qq.com>
2023-05-10 16:55:50 +0800

    取消finder组件固定宽度

--- src/components/ui/finder.vue
+++ src/components/ui/finder.vue
@@ -31,7 +31,7 @@
         clearable
         @select="handleSelect"
         @blur="handleBlur"
-        style="width: 220px;"
+        style="min-width: 220px;"
       >
         <el-button
           slot="append"
