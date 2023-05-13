dfb853e5325ddee57cc2f30c1c2b02dbaf8f817b
ho <hong349@qq.com>
2023-05-13 09:40:09 +0800

    解决冲突

--- src/components/common/list.vue
+++ src/components/common/list.vue
@@ -25,26 +25,9 @@
            </el-col>
     </el-row> -->
     <el-row v-show="!hideButtons" type="flex" class="row-bg" justify="space-between">
-<<<<<<< HEAD
-      <div class="table-head-btns">
-          <!-- <el-button type="primary" size="small"  v-if="(defaultDirtyFlags == 'add' || listType ==  'addchildlist') && batchAddButton && batchAddButton.hasOwnProperty('batchAdd') && batchAddButton.batchAdd.isDisp && selection" @click.stop="onMemBatchUpdateActive">
-            批量操作
-          </el-button> -->
-          <el-popover 
-            placement="right"
-            popper-class="table-popover"
-            width="800"
-            v-if="moreConfig && moreConfig.hasOwnProperty('table_explain')"
-            trigger="click">
-            <div v-html="moreConfig.table_explain.desc">{{moreConfig.table_explain.desc}}</div>
-            <div slot="reference" style="color:#525252;padding:2px 10px;">列表字段说明<i  class="el-icon-question"></i></div>
-          </el-popover>
-          
-        </div>
+
       
       
-      
-=======
         <div class="table-head-btns">
         <!-- <el-button type="primary" size="small"  v-if="(defaultDirtyFlags == 'add' || listType ==  'addchildlist') && batchAddButton && batchAddButton.hasOwnProperty('batchAdd') && batchAddButton.batchAdd.isDisp && selection" @click.stop="onMemBatchUpdateActive">
           批量操作
@@ -60,7 +43,6 @@
         </el-popover>
         
       </div>
->>>>>>> add7bc5b92fb29238097a04c912672744c1ea035
       <div class="table-head-btns">
         <template v-for="(item, index) in sortedGridButtons">
           <el-button :key="index" :size="item._moreConfig.size"
@@ -767,10 +749,7 @@ export default {
   line-clamp: 1;
   -webkit-box-orient: vertical;
 }
-<<<<<<< HEAD
-=======
 
->>>>>>> add7bc5b92fb29238097a04c912672744c1ea035
 .el-popper[x-placement^=right] .popper__arrow::after {
     bottom: -6px;
     left: 1px;
