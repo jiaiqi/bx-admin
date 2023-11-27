4037a2689973f3956c650f2572a764e3071a49fc
ho <hong349@qq.com>
2023-11-27 16:21:09 +0800

    已添加        src/pages/studentfiles/pdfReview.vue

---
+++ src/pages/studentfiles/pdfReview.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+      <button @click="exportPDF">导出PDF</button>
+    </div>
+  </template>
+   
+  <script>
+  import jsPDF from 'jspdf';
+   
+  export default {
+    methods: {
+      exportPDF() {
+        const doc = new jsPDF();
+        const elementHandler = {
+          '#ignorePDF': function(element, renderer) {
+            return true;
+          }
+        };
+        const source = window.document.getElementById('content').innerHTML;
+        doc.fromHTML(source, 15, 15, {
+          'width': 170,
+          'elementHandlers': elementHandler
+        });
+        doc.save('export.pdf');
+      }
+    }
+  }
+  </script>
