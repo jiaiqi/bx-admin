8415a6c43bf324a6bd4b9582626a213019791295
ho <hong349@qq.com>
2023-12-04 11:21:37 +0800

    已修改        src/pages/inform.vue

--- src/pages/inform.vue
+++ src/pages/inform.vue
@@ -20,7 +20,7 @@ export default {
             id: "",
             content: "",
             serviceName:'',
-            srvApp:''
+            srvApp:'',
             attachmentList: [],
         };
     },
