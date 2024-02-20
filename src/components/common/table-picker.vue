8707c97a98f48a1ca39c5c86809c568f95d9c11c
jiaqi <2468260248@qq.com>
2024-02-20 14:55:12 +0800

    下拉多选 字段的is_tree为true时才会展示为树形结构

--- src/components/common/table-picker.vue
+++ src/components/common/table-picker.vue
@@ -131,7 +131,6 @@ export default {
       return ["fks", "fkjsons"].includes(this.fieldType) || this.field.info.moreConfig?.multi === true;
     },
     isTree() {
-      return true
       return this.optionListV2?.is_tree === true
     },
     valueCol() {
