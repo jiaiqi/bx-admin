69f2917250816016a39456ae3c3dedb6d315af83
jiaqi <2468260248@qq.com>
2025-03-11 14:48:44 +0800

    修复 样式中配置了rpx导致在pc端不生效

--- src/pages/datav/common/index.js
+++ src/pages/datav/common/index.js
@@ -2,15 +2,17 @@ import { getImagePath } from "./http";
 
 // 处理后端返回的样式数据
 const formatStyleData = (json) => {
-  const str = JSON.stringify(json);
+  let str = JSON.stringify(json);
   if (!isJSON(str)) return {};
-
   let obj = {};
   for (let key in json) {
     let _key = key.replace("_", "-");
     obj[_key] = json[key];
     if (_key === "background-image") {
       obj[_key] = `url(${getImagePath(json[key])})`;
+    }
+    if(typeof obj[_key]==='string' && obj[_key].includes('rpx')){
+      obj[_key] = rpx2px(obj[_key]);
     }
   }
   if (obj["background-image"] && !obj["background-size"]) {
