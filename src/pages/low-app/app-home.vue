6081519d080bac944d3948f75f9f8140189835f1
jiaqi <2468260248@qq.com>
2025-06-06 15:11:43 +0800

    修复小程序设计器预览链接不正确

--- src/pages/low-app/app-home.vue
+++ src/pages/low-app/app-home.vue
@@ -161,7 +161,7 @@ export default{
     //预览
     showPreview(){
 
-      const url = process.env.NODE_ENV === "development"?`http://192.168.0.28/xmp/views/custom/index/index?page_no=${this.pageNo}`:`xmp/views/custom/index/index?page_no=${this.pageNo}`;
+      const url = process.env.NODE_ENV === "development"?`http://192.168.0.28/xmp/views/custom/index/index?page_no=${this.pageNo}`:`/xmp/views/custom/index/index?page_no=${this.pageNo}`;
       window.open(url, "_blank");
     },
     //容器收缩板
