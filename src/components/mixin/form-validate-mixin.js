f5a28420859fac37c434a53b5eecf03e460403b6
jiaqi <2468260248@qq.com>
2025-07-29 14:43:36 +0800

    fix(表单验证): 修复只读字段验证逻辑并优化验证规则处理
    - 在Field.js中添加只读字段检查，防止验证只读字段
    - 在form-validate-mixin.js中优化验证规则处理逻辑，仅验证可编辑字段
    - 修复验证状态处理和错误提示逻辑
    - 清理冗余代码并统一代码风格

--- src/components/mixin/form-validate-mixin.js
+++ src/components/mixin/form-validate-mixin.js
@@ -5,9 +5,11 @@ export default {
       let rulesMap = {};
       for (let key in this.fields) {
         let field = this.fields[key];
-        // if (field.evalEditable() && field.info.rules) {
-        if (field.info.rules) {
-          rulesMap[field.info.name] = field.info.rules;
+        if (field.evalEditable() && field.info.rules) {
+          // 只有可编辑的字段才进行校验
+          if (field.info.rules) {
+            rulesMap[field.info.name] = field.info.rules;
+          }
         }
       }
 
@@ -28,7 +30,7 @@ export default {
      * @returns {Promise<any>}
      */
     validateForm: function () {
-      return new Promise( (resolve, reject) => {
+      return new Promise((resolve, reject) => {
         Object.keys(this.allFields).forEach(fieldName => this.handleValidation(fieldName));
         let visibleBadFields = Object.values(this.allFields).filter(field => field.evalVisible() && field.hasValidateError());
         let ok = visibleBadFields.length === 0;
@@ -46,18 +48,18 @@ export default {
         let ruleArray = fieldRules[fieldName];
         ruleArray.forEach(rule => {
           // console.log('',field)
-          let state = this.evalFieldRule(rule,field,fieldName,this);
-          if( (state === true)||(typeof state==='object')){
+          let state = this.evalFieldRule(rule, field, fieldName, this);
+          if ((state === true) || (typeof state === 'object')) {
             field.clearValidateError(rule.name)
-          }else{
-            console.log('put Validate Error else:',rule.name, rule.message)
+          } else {
+            console.log('put Validate Error else:', rule.name, rule.message)
             field.putValidateError(rule.name, rule.message)
           }
           // (state === true)||(typeof state==='object') ?  : field.putValidateError(rule.name, rule.message);
-          if(typeof state==='object'&&state.type==='confirm'){
-            if(state.value===true||state.value==='true'){
+          if (typeof state === 'object' && state.type === 'confirm') {
+            if (state.value === true || state.value === 'true') {
               field.clearValidatePrompt(rule.name);
-            }else if(typeof state.value==='string'&&state.value!=='true'){
+            } else if (typeof state.value === 'string' && state.value !== 'true') {
               field.putValidatePrompt(rule.name, rule.message)
             }
           }
@@ -75,7 +77,7 @@ export default {
       if (validator.js) {
         // noinspection JSUnusedLocalSymbols
         let form = this;
-        
+
         let row = this.srvValFormModel();
         ret = eval("var zz=" + validator.js + "(row,form); zz");
       } else if (validator.func) {
@@ -106,18 +108,18 @@ export default {
      * @param field
      * @returns {boolean} true for validate ok,
      */
-    evalFieldRule(rule, field,fieldName,self) {
+    evalFieldRule(rule, field, fieldName, self) {
       let fieldSrvVal = field.getSrvVal();
-      if(rule.name==='isValidValue'){
-        if(field.finderSelected&&!fieldSrvVal){
+      if (rule.name === 'isValidValue') {
+        if (field.finderSelected && !fieldSrvVal) {
           return {
-            value:'false',
-            type:'confirm'
+            value: 'false',
+            type: 'confirm'
           }
-        }else{
+        } else {
           return {
-            value:'true',
-            type:'confirm'
+            value: 'true',
+            type: 'confirm'
           }
         }
       }
@@ -132,43 +134,43 @@ export default {
       } else if (rule.hasOwnProperty("required")) {
         // console.log("eval Field Rule",field.info.label)
         return fieldSrvVal !== null && fieldSrvVal !== undefined && fieldSrvVal !== "";
-      } else if(rule.hasOwnProperty("js_validate")){
-        if(self.fields[fieldName].evalXIf()){ //如果显示当前字段
+      } else if (rule.hasOwnProperty("js_validate")) {
+        if (self.fields[fieldName].evalXIf()) { //如果显示当前字段
           var data = self.srvValFormModel()
           let result = null
-          if(rule.js_validate){
+          if (rule.js_validate) {
             let js_validate = rule.js_validate
             let approval = null // 流程审批结果表单
-            if(self.hasOwnProperty("approvalFormMode")){
+            if (self.hasOwnProperty("approvalFormMode")) {
               approval = self.approvalFormMode
             }
-            
-            result= eval('('+ js_validate + ')(data,approval)')
-            if(typeof result ==='object'){
-              if(result.hasOwnProperty('type')&&result.hasOwnProperty('value')){
-                if(result.type==='validate'){
+
+            result = eval('(' + js_validate + ')(data,approval)')
+            if (typeof result === 'object') {
+              if (result.hasOwnProperty('type') && result.hasOwnProperty('value')) {
+                if (result.type === 'validate') {
                   //警告 校验不通过将不能提交表单
-                  if(typeof result.value==='string'&&result.value!=='true'){
-                    rule.message=result.value
+                  if (typeof result.value === 'string' && result.value !== 'true') {
+                    rule.message = result.value
                     return false
-                  }else if(result.value===true||result.value==='true'){
+                  } else if (result.value === true || result.value === 'true') {
                     return true
                   }
-                }else if(result.type==='confirm'){
+                } else if (result.type === 'confirm') {
                   //提示 不影响表单提交
                   // TODO 增加提示文字
-                  if(typeof result.value==='string'&&result.value!=='true'){
-                    rule.message=result.value
+                  if (typeof result.value === 'string' && result.value !== 'true') {
+                    rule.message = result.value
                   }
                   return result
                 }
               }
-            }else{
+            } else {
               // 按旧版本validate特性规则处理
-              if(result===true){
+              if (result === true) {
                 return result
-              }else if(typeof result === 'string'){
-                rule.message=result
+              } else if (typeof result === 'string') {
+                rule.message = result
                 return false
               }
             }
@@ -183,11 +185,11 @@ export default {
             //     return false
             //   }
             // }
-          }else{
+          } else {
             return true
           }
         }
-      }else if (rule.hasOwnProperty("max")) {
+      } else if (rule.hasOwnProperty("max")) {
         if (field.info.isNumeric()) {
           let number = Number.parseFloat(fieldSrvVal);
           return number <= rule.max;
@@ -195,20 +197,20 @@ export default {
           let text = fieldSrvVal;
           return !text || text.length <= rule.max;
         }
-      } else if(rule.hasOwnProperty("isValidFun")){
-         // 通知 dynamic-sub-temp.vue 组件动态通知
-          VueEvent.$emit('to-verify',true)
-          if(rule.hasOwnProperty("valid")){
-            console.log("rule.valid",rule.valid)
-            return rule.valid
-          }
-      }else if(rule.encode==='en'&&rule.name==='ngMaxlength'&&rule.ngMaxlength){
-        if(fieldSrvVal&&this.calcCharLength(fieldSrvVal)>Number(rule.ngMaxlength)){
+      } else if (rule.hasOwnProperty("isValidFun")) {
+        // 通知 dynamic-sub-temp.vue 组件动态通知
+        VueEvent.$emit('to-verify', true)
+        if (rule.hasOwnProperty("valid")) {
+          console.log("rule.valid", rule.valid)
+          return rule.valid
+        }
+      } else if (rule.encode === 'en' && rule.name === 'ngMaxlength' && rule.ngMaxlength) {
+        if (fieldSrvVal && this.calcCharLength(fieldSrvVal) > Number(rule.ngMaxlength)) {
           rule.message = `最多输入${rule.ngMaxlength}个字符,一个汉字占两个字符`
           return false
         }
         return true
-      }else{
+      } else {
         // TODO: handle min
         return true;
       }
@@ -218,28 +220,28 @@ export default {
    * @param {*} val 
    * @returns {number}
    */
-  calcCharLength(val=''){
-    val = val + ''
-    //中文、中文标点、全角字符按1长度，英文、英文符号、数字按0.5长度计算
-    let cnReg = /([\u4e00-\u9fa5]|[\u3000-\u303F]|[\uFF00-\uFF60])/g;
-    let mat = val.match(cnReg);
-    let length = 0;
-    if (mat) {
-      length = mat.length*2 + (val.length - mat.length);
-    } else {
-      length = val.length;
+    calcCharLength(val = '') {
+      val = val + ''
+      //中文、中文标点、全角字符按1长度，英文、英文符号、数字按0.5长度计算
+      let cnReg = /([\u4e00-\u9fa5]|[\u3000-\u303F]|[\uFF00-\uFF60])/g;
+      let mat = val.match(cnReg);
+      let length = 0;
+      if (mat) {
+        length = mat.length * 2 + (val.length - mat.length);
+      } else {
+        length = val.length;
+      }
+      return length
+      // var v = val;
+      // if(v == "") return true;
+      // var len = 0 ;
+      // for (var i = 0; i < v.length; i++) {
+      //     // 一个中文算两个字符
+      //     var c = v.charCodeAt(i) > 255 ? 2 : 1;
+      //     len += c;
+      // }
+      // return len
     }
-    return length
-    // var v = val;
-    // if(v == "") return true;
-    // var len = 0 ;
-    // for (var i = 0; i < v.length; i++) {
-    //     // 一个中文算两个字符
-    //     var c = v.charCodeAt(i) > 255 ? 2 : 1;
-    //     len += c;
-    // }
-    // return len
-  }
 
 
   },
